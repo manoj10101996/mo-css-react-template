1233bca959abd1cfe53dc2e7d1785ad28d4846b5
Manojkumar Muthukumar <manoj10101996@gmail.com>
2023-01-22 17:08:57 +0530

    Mo css react Template

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.beforepost.com/css/introduction"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <img
+            src={process.env.PUBLIC_URL + "logo192.png"}
+            className="App-logo"
+            alt="logo"
+          />
+          <div className="mo-text-b mb-5">MO CSS</div>
+          <div className="p-0 mb-3 mo-text-i mo-text-3">
+            The new generation front-end framework for web applications.
+          </div>
+          <div className="mo-text-2">Learn More About Beforepost Mo CSS</div>
         </a>
+        <div className="mo-alert mo-border mo-border-danger mo-text-2 mo-alert-bg-danger mt-4">
+          https://cdn.beforepost.com/neptune/beforepost-0.2.min.css
+        </div>
       </header>
     </div>
   );
